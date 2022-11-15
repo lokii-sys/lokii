deabd026d8756037711462f18f97088c4972ab1c
LOKII Arduino Shield <lokii@centek.com.hk>
2022-11-15 14:06:52 +0800

    Add files via upload
    LOKII-CE  Video streaming demo

---
+++ examples/videoStream/videoStream.ino
@@ -0,0 +1,57 @@
+#include <lokii.h>
+
+#define BLOCKING 1
+#define NOBLOCKING 0
+
+   
+LOKII lokii;
+
+// WIFI
+uint8_t wifiStatus[2] = {0, 0};
+
+void readWifiStatus(int prtAnyway)
+{
+    uint8_t router, cloud;
+      
+    lokii.getWIFI(&router, &cloud);
+    if ( prtAnyway == 1 || wifiStatus[0] != router || wifiStatus[1] != cloud)
+    {
+        wifiStatus[0] = router;
+        wifiStatus[1] = cloud;
+        Serial.printf("\nwifi status : %d  %d \n", wifiStatus[0], wifiStatus[1]);
+    }
+}
+
+
+void setup(){
+  
+  Serial.begin(9600);
+  //while(!Serial);  
+
+  lokii.connect();  
+}
+
+void loop(){
+  
+  uint8_t router, cloud;
+
+  lokii.playTTS("video streaming Demo" , L_DEFAULT ,5 ,5 ,E_NATURAL);
+  lokii.setCameraMode(L_CAM_V_STREAM_1280X720); // 10 seconds 
+
+  lokii.setWIFI(1, 1);                    
+  Serial.println("Connecting WIFI and Cloud");
+  while ( wifiStatus[0] != 1)
+  {
+      readWifiStatus(0);    
+  }
+  Serial.printf("\nwifi status : %d  %d \n", wifiStatus[0], wifiStatus[1]);
+       
+  delay(60000);
+
+  lokii.setCameraMode(L_CAM_V_STREAM_STOP);
+  lokii.playTTS("video streaming stop" , L_DEFAULT ,5 ,5 ,E_NATURAL);
+
+  
+  while (1)
+      delay(1000);
+}
